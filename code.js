987677425618730de555a5ff7106293c72221142
Behnam <behnamborhani95@gmail.com>
2023-11-18 11:54:44 +0330

    refactor gte function

--- code.js
+++ code.js
@@ -1,7 +1,12 @@
 function gte(a, b) {
-  if (a > b) return true;
+  /* if (a > b) return true;
   if (a < b) return false;
-  if (a == b) return true;
+  if (a == b) return true; */
+
+  if (a >= b) return true;
+  return false;
+
+//   return a >= b ? true : false;
 }
 
 module.exports = gte;
