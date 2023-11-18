f3ed0bd47556a6e2d4d6bee9096576896d26aa69
Behnam <behnamborhani95@gmail.com>
2023-11-18 11:40:22 +0330

    write test for gte

--- test/code.test.js
+++ test/code.test.js
@@ -1,5 +1,16 @@
-test("our first test", () => {});
+const gte = require("../code");
 
-test("our second test", () => {
-  throw new Error("Something faild");
+test("gte - should return true if a is greater than b", () => {
+  const result = gte(10, 5);
+  expect(result).toBe(true);
+});
+
+test("gte - should return false if a is less than b", () => {
+  const result = gte(5, 10);
+  expect(result).toBe(false);
+});
+
+test("gte - should return true if a is equal to b", () => {
+  const result = gte(5, 5);
+  expect(result).toBe(true);
 });
