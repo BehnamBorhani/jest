2b686763310b8131b97d7dd3df9c7e93a29540d6
Behnam <behnamborhani95@gmail.com>
2023-11-18 11:46:06 +0330

    grouped gte tests with describe func

--- test/code.test.js
+++ test/code.test.js
@@ -1,16 +1,18 @@
 const gte = require("../code");
 
-test("gte - should return true if a is greater than b", () => {
-  const result = gte(10, 5);
-  expect(result).toBe(true);
-});
+describe("gte", () => {
+  it("should return true if a is greater than b", () => {
+    const result = gte(10, 5);
+    expect(result).toBe(true);
+  });
 
-test("gte - should return false if a is less than b", () => {
-  const result = gte(5, 10);
-  expect(result).toBe(false);
-});
+  it("should return false if a is less than b", () => {
+    const result = gte(5, 10);
+    expect(result).toBe(false);
+  });
 
-test("gte - should return true if a is equal to b", () => {
-  const result = gte(5, 5);
-  expect(result).toBe(true);
+  it("should return true if a is equal to b", () => {
+    const result = gte(5, 5);
+    expect(result).toBe(true);
+  });
 });
